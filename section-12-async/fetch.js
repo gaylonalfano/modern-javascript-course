fc91ebbe37f74822963df71d6670029ed5b3bb7b
gaylonalfano <gaylon.alfano@gmail.com>
2022-02-20 16:52:50 +0800

    More exploration with async/await and Promise.all().

--- section-12-async/fetch.js
+++ section-12-async/fetch.js
@@ -1,9 +1,25 @@
 // Async & Await
-// async function getTodos() {
-//   // NOTE ALL async functions return a Promise, no matter what
-//   // we do inside this async function!
-//   const response = await fetch("data/mario.json");
-// }
+async function getTodos() {
+  // NOTE ALL async functions return a Promise, no matter what
+  // we do inside this async function!
+  // NOTE 'await' does block but bc this is all within an ASYNC
+  // function, when we call it outside it will not block!
+  const response = await fetch("data/mario.json"); // Blocking
+  const data = await response.json(); // Blocking
+
+  return data;
+}
+
+// Demonstrates that calling async getTodos() is non-blocking
+console.log(1);
+console.log(2);
+
+getTodos()
+  .then((data) => console.log(data))
+  .catch((error) => console.log(error));
+
+console.log(3);
+console.log(4); // 1,2,3,4, [{...}, {...}, {...}]
 
 // // Promise Chaining (standard)
 // fetch("data/mariooo.json")
@@ -24,58 +40,82 @@
 //     console.log("REJECTED", err);
 //   });
 
-// === Promise.all([Promise1, Promise2, ...])
-// https://youtu.be/vn3tm0quoqE?t=545
-async function getFruit(name) {
-  const fruits = {
-    pineapple: "pineapple",
-    peach: "peach",
-    strawberry: "strawberry",
-  };
+// // === Promise.all([Promise1, Promise2, ...])
+// // https://youtu.be/vn3tm0quoqE?t=545
+// async function getFruit(name) {
+//   const fruits = {
+//     pineapple: "pineapple",
+//     peach: "peach",
+//     strawberry: "strawberry",
+//   };
 
-  // NOTE 'async' keyword basically syntax sugar for:
-  // return Promise.resolve(fruits[name])
-  return fruits[name];
-}
+//   // NOTE 'async' keyword basically syntax sugar for:
+//   // return Promise.resolve(fruits[name])
+//   return fruits[name];
+// }
 
-getFruit("peach").then(console.log);
+// getFruit("peach").then(console.log);
 
-async function makeSmoothie() {
-  // BAD - This is BLOCKING! Only do if next line depends on this value!
-  // const a = await getFruit("pineapple"); // BLOCKING!
-  // const b = await getFruit("strawberry");
-  // return [a, b];
+// async function makeSmoothie() {
+//   // BAD - This is BLOCKING! Only do if next line depends on this value!
+//   // const a = await getFruit("pineapple"); // BLOCKING!
+//   // const b = await getFruit("strawberry");
+//   // return [a, b];
 
-  // BETTER - Add Promises to array and at end return Promise.all([Promise1, Promise2, ...])
-  // const a = getFruit("pineapple"); // Promise
-  // const b = getFruit("strawberry"); // Promise
-  // return Promise.all([a, b]); // ['pineapple', 'strawberry']
+//   // BETTER - Add Promises to array and at end return Promise.all([Promise1, Promise2, ...])
+//   // const a = getFruit("pineapple"); // Promise
+//   // const b = getFruit("strawberry"); // Promise
+//   // return Promise.all([a, b]); // ['pineapple', 'strawberry']
 
-  // BEST - Use 'await' for the Promise.all() call
-  const a = getFruit("pineapple");
-  const b = getFruit("strawberry");
-  const smoothie = await Promise.all([a, b]);
-  return smoothie; // ['pineapple', 'strawberry']
-}
-makeSmoothie().then(console.log);
+//   // BEST - Use 'await' for the Promise.all() call
+//   const a = getFruit("pineapple");
+//   const b = getFruit("strawberry");
+//   const smoothie = await Promise.all([a, b]);
+//   return smoothie; // ['pineapple', 'strawberry']
+// }
+// makeSmoothie().then(console.log);
 
-// = Can even use await inside FOR LOOPS
-const fruits = ["peach", "pineapple", "strawberry"];
-const smoothie = fruits.map((v) => getFruit(v));
-const fruitLoop = async () => {
-  // NOTE This will await the array of Promises (smoothie) to resolve
-  // and then loop over the array immediately afterwards to log
-  for await (const fruit of smoothie) {
-    console.log(fruit);
-  }
-};
-fruitLoop();
+// // = Can even use await inside FOR LOOPS
+// const fruits = ["peach", "pineapple", "strawberry"];
+// const smoothie = fruits.map((v) => getFruit(v));
+// const fruitLoop = async () => {
+//   // NOTE This will await the array of Promises (smoothie) to resolve
+//   // and then loop over the array immediately afterwards to log
+//   for await (const fruit of smoothie) {
+//     console.log(fruit);
+//   }
+// };
+// fruitLoop();
 
-// = Can use await directly inside IF STATEMENTS
-const fruitInspection = async () => {
-  // NOTE We can await the resolved value of a Promise to compare to another val
-  if ((await getFruit("peach")) === "peach") {
-    console.log("looks peachy!");
-  }
-};
-fruitInspection();
+// // = Can use await directly inside IF STATEMENTS
+// const fruitInspection = async () => {
+//   // NOTE We can await the resolved value of a Promise to compare to another val
+//   if ((await getFruit("peach")) === "peach") {
+//     console.log("looks peachy!");
+//   }
+// };
+// fruitInspection();
+
+// === Looping over multiple resources and Promise.all()
+// const tick = Date.now();
+// const log = (v) => console.log(`${v} \n Elapsed: ${Date.now() - tick}`);
+
+// async function getTodos() {
+//   const resources = ["data/mario.json", "data/peach.json", "data/toad.json"];
+
+//   try {
+//     // Trying to run concurrently so want to use await Promise.all(Promise[]);
+//     const results = await Promise.all(
+//       resources.map((resource) =>
+//         fetch(resource).then((response) => response.json())
+//       )
+//     );
+//     console.log("results: ", results); // [Array(3), Array(3), Array(4)]
+//   } catch (err) {
+//     console.log(err);
+//   }
+// }
+
+// log("start");
+// getTodos();
+// log("finish");
